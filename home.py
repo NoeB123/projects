9368714b9e3e2805c1d52905f6317b40cef0f1cb
Bertramo Noe <noe.bertramo@student-cs.fr>
2025-05-03 18:08:54 +0200

    Modified for streamlit frontend

---
+++ home.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.write("# Noe Bertramo: Data Science Portfolio")
+st.write("MIT Master's in Data Science Artificial Intelligence")
+st.write("CentraleSupélec Master's in Applied Mathematics, Computer Science")
+
+
+st.write("## Projects")
+
+st.write("## Interests")
+
+st.write("## Links") 
+
+st.write("## Fun Stuff")
+
+st.write("## Contact")
+
+st.write("Feel free to reach out to me at: \t_noe_bert at mit.edu_")
